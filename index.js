508041c49eb96939079bf051c5b35cb93b8cad68
darling-token-6272 <titleyugio@gmail.com>
2018-12-21 18:40:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log();
+}
+function logWhisper(string) {
+  console.log();
+}
+var uppercase = "HELLO!";
+uppercase.toUppCase() === uppercase // true;
+var lowercase = "hello!";
+lower.case.toLowerCase() === lowercase // true;
